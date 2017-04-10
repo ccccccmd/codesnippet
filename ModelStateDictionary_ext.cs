3ea6c48d290961c11d6ec0aa59a127aab7a03187
ccccccmd <ccccccmd@163.com>
2017-04-10 19:28:23 +0800

    model_state_ext

---
+++ ModelStateDictionary_ext.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+using System.Web.ModelBinding;
+
+namespace WEDO.Common.Extension
+{
+    public static class ModelStateExtension
+    {
+        /// <summary>
+        ///     获得错误信息
+        /// </summary>
+        /// <param name="modelState"></param>
+        /// <returns></returns>
+        public static string GetErrorMsg(this ModelStateDictionary modelState)
+        {
+            if (modelState.IsValid || !modelState.Values.Any())
+            {
+                return string.Empty;
+            }
+
+            foreach (var m in modelState.Values)
+            {
+                if (m.Errors.Any())
+                {
+                    return m.Errors[0].ErrorMessage;
+                }
+            }
+            return string.Empty;
+        }
+    }
+}
